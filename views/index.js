abd55db868fe29f9b4998954d360163c630435fd
josephquested <josephquested@gmail.com>
2016-11-19 13:08:42 +1300

    add user and room count

--- views/index.js
+++ views/index.js
@@ -4,15 +4,25 @@ function render (data) {
   return `
     ${header()}
     <div id="rooms">
-
-      <div class="room-container">
-        <div class="room-content">
-          <h3>test room title</h3>
-        </div>
-      </div>
-
+      ${renderRooms()}
+    </div>
     <script src="/bundle.js"></script>
   `
+}
+
+function renderRooms () {
+  var html = ''
+  io.rooms.forEach((room) => {
+    html += `
+      <div class="room-container">
+        <div class="room-content">
+          <h3>${room.name}</h3>
+          <h2>${room.genre}</h2>
+        </div>
+      </div>
+    `
+  })
+  return html
 }
 
 module.exports = render
--- views/partials/header.js
+++ views/partials/header.js
@@ -1,12 +1,12 @@
 function render (data) {
   return `
     <div id="header">
-      <h1>steamroom_</h1>
+      <a id="header-link" href="/"><h1>steamroom_</h1></a>
       <h2 class="inline">
-        <p id="guest-quantity" class="inline">0</p> guests
+        <p id="guest-quantity" class="inline">${io.engine.clientsCount}</p> guests
       </h2>
       <h2 class="inline">
-        <p id="room-quantity" class="inline">0</p> rooms
+        <p id="room-quantity" class="inline">${io.rooms.length}</p> rooms
       </h2>
       <hr>
       <input placeholder="search rooms">
