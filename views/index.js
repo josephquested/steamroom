c4a5695131af6f3a7dc24d7e0ebdb33c4a09ceb0
josephquested <josephquested@gmail.com>
2016-11-21 14:16:00 +1300

    minior refactor

--- views/index.js
+++ views/index.js
@@ -1,25 +1,36 @@
 var header = require('./partials/header')
 
+var dummyRooms = [
+  {name: "Dexter Quested Convos", genre: "children"},
+  {name: "Robots", genre: "technology"},
+  {name: "Was Hitler right?", genre: "history"},
+  {name: "Best Cheers Episdoe..", genre: "tv"},
+  {name: "I am homeless", genre: "lifestyle"}
+]
+
 function render (data) {
   return `
     ${header()}
     <div id="rooms">
-      ${renderRooms()}
+      ${renderRooms(io.rooms)}
+      ${renderRooms(dummyRooms)}
     </div>
     <script src="/bundle.js"></script>
   `
 }
 
-function renderRooms () {
+function renderRooms (rooms) {
   var html = ''
-  io.rooms.forEach((room) => {
+  rooms.forEach((room) => {
     html += `
-      <div class="room-container">
-        <div class="room-content">
-          <h3>${room.name}</h3>
-          <h2>${room.genre}</h2>
+      <a class="room-link" href="room/?id=${room.id}">
+        <div class="room-container">
+          <div class="room-content">
+            <h2 class="room-link-name">${room.name}</h3>
+            <h3 class="room-link-genre">${room.genre}</h2>
+          </div>
         </div>
-      </div>
+      </a>
     `
   })
   return html
--- views/templits/index.js
+++ views/templits/index.js
@@ -5,6 +5,8 @@ module.exports = (body) => {
       <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0, maximum-scale=1, minimum-scale=1">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <link rel="stylesheet" href="../index.css">
         <title>steamroom_</title>
       </head>
